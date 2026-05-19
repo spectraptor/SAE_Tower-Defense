c5e07763a87cd8db74776d1e632329722aada90e
CHEEMA Mohammad <mcheema>
2026-05-19 14:07:35 +0200

    Initialisation d'une GameLoop de début

--- src/main/java/universite_paris8/iut/mcheema/codesource/controleur/Controleur.java
+++ src/main/java/universite_paris8/iut/mcheema/codesource/controleur/Controleur.java
@@ -1,5 +1,7 @@
 package universite_paris8.iut.mcheema.codesource.controleur;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
@@ -9,6 +11,9 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.TilePane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.util.Duration;
 import universite_paris8.iut.mcheema.codesource.modele.Environnement;
 import universite_paris8.iut.mcheema.codesource.modele.Terrain;
 import universite_paris8.iut.mcheema.codesource.vue.TerrainVue;
@@ -17,13 +22,15 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Controleur implements Initializable {
+    private Timeline gameLoop;
+    private int temps;
+    private Circle leCercle = new Circle(5, Color.RED);
+
     private Environnement environnement;
 
     @FXML
     private TilePane tilePane;
 
-    @FXML
-    private BorderPane borderPanePrincipal;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -31,5 +38,34 @@ public class Controleur implements Initializable {
         this.tilePane.setPrefSize(this.environnement.getTerrainDeJeu().obtenirLargeur()* Terrain.PIXEL_TUILLE,this.environnement.getTerrainDeJeu().obtenirHauteur()*Terrain.PIXEL_TUILLE);
         TerrainVue terrainVue = new TerrainVue(this.environnement.getTerrainDeJeu(),this.tilePane);
         terrainVue.afficheTerrainJeu();
+        this.initAnimation();
+        this.gameLoop.play();
+    }
+
+    private void initAnimation() {
+        gameLoop = new Timeline();
+        temps=0;
+        gameLoop.setCycleCount(Timeline.INDEFINITE);
+
+        KeyFrame kf = new KeyFrame(
+                // on définit le FPS (nbre de frame par seconde)
+                Duration.seconds(0.017),
+                // on définit ce qui se passe à chaque frame
+                // c'est un eventHandler d'ou le lambda
+                (ev ->{
+                    if(temps==100){
+                        System.out.println("fini");
+                        gameLoop.stop();
+                    }
+                    else if (temps%5==0){
+                        System.out.println("un tour");
+                        leCercle.setLayoutX(leCercle.getLayoutX()+5);
+                        leCercle.setLayoutY(leCercle.getLayoutY()+5);
+
+                    }
+                    temps++;
+                })
+        );
+        gameLoop.getKeyFrames().add(kf);
     }
 }
