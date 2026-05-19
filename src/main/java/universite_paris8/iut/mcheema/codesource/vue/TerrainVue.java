1daaa98081f53c30ae8e4f7d636679c6780141e7
Valentin Huang <vhuang@iut.univ-paris8.fr>
2026-05-19 16:24:32 +0200

    Ajout des textures de chemins sur les 3 terrains

--- src/main/java/universite_paris8/iut/mcheema/codesource/vue/TerrainVue.java
+++ src/main/java/universite_paris8/iut/mcheema/codesource/vue/TerrainVue.java
@@ -18,9 +18,49 @@ public class TerrainVue {
         Image imgHerbe = new Image(getClass().getResource("/universite_paris8/iut/mcheema/codesource/texture/herbe.png").toExternalForm());
         Image imgTerre = new Image(getClass().getResource("/universite_paris8/iut/mcheema/codesource/texture/terre.png").toExternalForm());
         Image imgEau = new Image(getClass().getResource("/universite_paris8/iut/mcheema/codesource/texture/eau.png").toExternalForm());
+        Image ch_gauche_droite = new Image(getClass().getResource("/universite_paris8/iut/mcheema/codesource/texture/chemins/chemin1.png").toExternalForm());
+        Image ch_haut_bas = new Image(getClass().getResource("/universite_paris8/iut/mcheema/codesource/texture/chemins/chemin2.png").toExternalForm());
+        Image ch_droite_bas = new Image(getClass().getResource("/universite_paris8/iut/mcheema/codesource/texture/chemins/chemin3.png").toExternalForm());
+        Image ch_bas_gauche = new Image(getClass().getResource("/universite_paris8/iut/mcheema/codesource/texture/chemins/chemin4.png").toExternalForm());
+        Image ch_gauche_haut = new Image(getClass().getResource("/universite_paris8/iut/mcheema/codesource/texture/chemins/chemin5.png").toExternalForm());
+        Image ch_haut_droite = new Image(getClass().getResource("/universite_paris8/iut/mcheema/codesource/texture/chemins/chemin6.png").toExternalForm());
+        Image ch_haut_droite_bas = new Image(getClass().getResource("/universite_paris8/iut/mcheema/codesource/texture/chemins/chemin7.png").toExternalForm());
+        Image ch_droite_bas_gauche = new Image(getClass().getResource("/universite_paris8/iut/mcheema/codesource/texture/chemins/chemin8.png").toExternalForm());
+        Image ch_bas_gauche_haut = new Image(getClass().getResource("/universite_paris8/iut/mcheema/codesource/texture/chemins/chemin9.png").toExternalForm());
+        Image ch_gauche_haut_droite = new Image(getClass().getResource("/universite_paris8/iut/mcheema/codesource/texture/chemins/chemin10.png").toExternalForm());
         for (int i = 0; i < this.terrainDeJeu.obtenirHauteur(); i++) {
             for (int j = 0; j < this.terrainDeJeu.obtenirLargeur(); j++) {
                 switch (this.terrainDeJeu.avoirCodeTuile(i,j)) {
+                    case '1':
+                        this.tilePane.getChildren().add(new ImageView(ch_gauche_droite));
+                        break;
+                    case '2':
+                        this.tilePane.getChildren().add(new ImageView(ch_haut_bas));
+                        break;
+                    case '3':
+                        this.tilePane.getChildren().add(new ImageView(ch_droite_bas));
+                        break;
+                    case '4':
+                        this.tilePane.getChildren().add(new ImageView(ch_bas_gauche));
+                        break;
+                    case '5':
+                        this.tilePane.getChildren().add(new ImageView(ch_gauche_haut));
+                        break;
+                    case '6':
+                        this.tilePane.getChildren().add(new ImageView(ch_haut_droite));
+                        break;
+                    case '7':
+                        this.tilePane.getChildren().add(new ImageView(ch_haut_droite_bas));
+                        break;
+                    case '8':
+                        this.tilePane.getChildren().add(new ImageView(ch_droite_bas_gauche));
+                        break;
+                    case '9':
+                        this.tilePane.getChildren().add(new ImageView(ch_bas_gauche_haut));
+                        break;
+                    case '0':
+                        this.tilePane.getChildren().add(new ImageView(ch_gauche_haut_droite));
+                        break;
                     case 'h':
                         this.tilePane.getChildren().add(new ImageView(imgHerbe));
                         break;
