a6042238f173b6fd0ae757c7621c73d2e3e20018
CHEEMA Mohammad <mcheema>
2026-05-19 15:38:54 +0200

    Modification de la constante PIXEL_TUILLE en TAILLE_TUILLE

--- src/main/java/universite_paris8/iut/mcheema/codesource/controleur/Controleur.java
+++ src/main/java/universite_paris8/iut/mcheema/codesource/controleur/Controleur.java
@@ -41,7 +41,7 @@ public class Controleur implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         this.environnement = new Environnement(1,640,480);
-        this.tilePane.setPrefSize(this.environnement.getTerrainDeJeu().obtenirLargeur()* Terrain.PIXEL_TUILLE,this.environnement.getTerrainDeJeu().obtenirHauteur()*Terrain.PIXEL_TUILLE);
+        this.tilePane.setPrefSize(this.environnement.getTerrainDeJeu().obtenirLargeur()* Terrain.TAILLE_TUILLE,this.environnement.getTerrainDeJeu().obtenirHauteur()*Terrain.TAILLE_TUILLE);
         TerrainVue terrainVue = new TerrainVue(this.environnement.getTerrainDeJeu(),this.tilePane);
         Ennemi bug = new Bogue(400, 100, this.environnement);
         this.environnement.ajouterEnnemi(bug);
