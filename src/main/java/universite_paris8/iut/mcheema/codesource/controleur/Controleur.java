569f738225b2a4062f42856adb6e8e89e56580c1
Pansan Abdel-Malik <apansan@iut.univ-paris8.fr>
2026-05-21 10:54:41 +0200

    Création ennemiVue lorsque le controleur est instancié.

--- src/main/java/universite_paris8/iut/mcheema/codesource/controleur/Controleur.java
+++ src/main/java/universite_paris8/iut/mcheema/codesource/controleur/Controleur.java
@@ -6,13 +6,12 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.TilePane;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
 import universite_paris8.iut.mcheema.codesource.modele.Bogue;
 import universite_paris8.iut.mcheema.codesource.modele.Ennemi;
 import javafx.util.Duration;
 import universite_paris8.iut.mcheema.codesource.modele.Environnement;
-import universite_paris8.iut.mcheema.codesource.modele.*;
+import universite_paris8.iut.mcheema.codesource.modele.Terrain;
+import universite_paris8.iut.mcheema.codesource.vue.EnnemiVue;
 import universite_paris8.iut.mcheema.codesource.vue.TerrainVue;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -39,10 +38,13 @@ public class Controleur implements Initializable {
         this.environnement = new Environnement(1);
         this.tilePane.setPrefSize(this.environnement.getTerrainDeJeu().obtenirLargeur()* Terrain.TAILLE_TUILLE,this.environnement.getTerrainDeJeu().obtenirHauteur()*Terrain.TAILLE_TUILLE);
         TerrainVue terrainVue = new TerrainVue(this.environnement.getTerrainDeJeu(),this.tilePane);
+
         Ennemi bug = new Bogue(400, 100, this.environnement); // (560, 140) si on veut essayer qu'il atteigne la fin
+        EnnemiVue bugVue =  new EnnemiVue(bug, paneJeu);
+
         this.environnement.ajouterEnnemi(bug);
-        creerSpriteEnnemi(bug);
         terrainVue.afficheTerrainJeu();
+
         this.initAnimation();
         this.gameLoop.play();
 
