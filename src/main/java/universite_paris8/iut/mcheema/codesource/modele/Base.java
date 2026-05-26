2ba75a911ebe4b99173cf5349c0a58857ceb0a76
CHEEMA Mohammad <mcheema>
2026-05-26 11:16:50 +0200

    Revert "Modification de la classe Base et création de la classe BaseVue"
    This reverts commit 6e3beb0659e0172f0a7bba52344a3e1192424225.

--- src/main/java/universite_paris8/iut/mcheema/codesource/modele/Base.java
+++ src/main/java/universite_paris8/iut/mcheema/codesource/modele/Base.java
@@ -5,13 +5,9 @@ import javafx.beans.property.SimpleIntegerProperty;
 
 public class Base {
     private IntegerProperty pvProperty;
-    private IntegerProperty xProperty;
-    private IntegerProperty yProperty;
 
-    public Base (int x,int y) {
+    public Base () {
         this.pvProperty = new SimpleIntegerProperty(100);
-        this.xProperty = new SimpleIntegerProperty(x);
-        this.yProperty = new SimpleIntegerProperty(y);
     }
 
     public final void setPv(int pv) {this.pvProperty.setValue(pv);}
@@ -19,38 +15,14 @@ public class Base {
 
     public final int getPv() {return this.pvProperty.getValue();}
 
-    public final IntegerProperty pvProperty() { return this.pvProperty;}
-
-    public final int getX() {
-        return this.xProperty.getValue();
-    }
-
-    public final void setX(int x) {
-        this.xProperty.setValue(x);
-    }
-
-    public final IntegerProperty xProperty() {
-        return this.xProperty;
-    }
-
-    public final int getY() {
-        return this.yProperty.getValue();
-    }
-
-    public final void setY(int y) {
-        this.yProperty.setValue(y);
-    }
-
-    public final IntegerProperty yProperty() {
-        return this.yProperty;
-    }
+    public final IntegerProperty getPvProperty() { return this.pvProperty;}
 
     public void subirDegats(int degats) {
-        this.setPv(this.getPv() - degats);
+        setPv(getPv() - degats);
     }
 
-    public boolean estDetruite() {
-        return this.getPv() <= 0;
+    public boolean estVivant() {
+        return getPv() > 0;
     }
 }
 
