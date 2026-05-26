dcf6e2d9d2e26ca1d8c59b9e11afed49914a5732
Valentin Huang <vhuang@iut.univ-paris8.fr>
2026-05-26 14:51:22 +0200

    Affichage du BFS dans le controleur (pour tester) et dans la console

--- src/main/java/universite_paris8/iut/mcheema/codesource/controleur/Controleur.java
+++ src/main/java/universite_paris8/iut/mcheema/codesource/controleur/Controleur.java
@@ -8,14 +8,12 @@ import javafx.scene.layout.Pane;
 import javafx.scene.layout.TilePane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
-import universite_paris8.iut.mcheema.codesource.modele.Bogue;
-import universite_paris8.iut.mcheema.codesource.modele.Ennemi;
+import universite_paris8.iut.mcheema.codesource.modele.*;
 import javafx.util.Duration;
-import universite_paris8.iut.mcheema.codesource.modele.Environnement;
-import universite_paris8.iut.mcheema.codesource.modele.Terrain;
 import universite_paris8.iut.mcheema.codesource.vue.TerrainVue;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class Controleur implements Initializable {
@@ -37,7 +35,21 @@ public class Controleur implements Initializable {
         this.environnement = new Environnement(1,640,480);
         this.tilePane.setPrefSize(this.environnement.getTerrainDeJeu().obtenirLargeur()* Terrain.TAILLE_TUILLE,this.environnement.getTerrainDeJeu().obtenirHauteur()*Terrain.TAILLE_TUILLE);
         TerrainVue terrainVue = new TerrainVue(this.environnement.getTerrainDeJeu(),this.tilePane);
-        Ennemi bug = new Bogue(400, 100, this.environnement); // (560, 140) si on veut essayer qu'il atteigne la fin
+        Ennemi bug = new Bogue(400, 100, this.environnement);
+
+        Sommet base = new Sommet(12, 0);
+        BFS bfs = new BFS(this.environnement.getTerrainDeJeu(), base);
+
+        Sommet depart = new Sommet(4, 19);
+
+        ArrayList<Sommet> chemin = bfs.cheminVersSource(depart);
+        System.out.println("Chemin BFS:");
+        for (Sommet s : chemin) {
+            System.out.println(
+                    "Ligne: " + s.getLigne() + " Colonne: " + s.getColonne()
+            );
+        }
+
         this.environnement.ajouterEnnemi(bug);
         creerSpriteEnnemi(bug);
         terrainVue.afficheTerrainJeu();
