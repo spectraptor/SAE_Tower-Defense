4a61d2b4179233d8655623d924816cf7b26f15fd
Pansan Abdel-Malik <apansan@iut.univ-paris8.fr>
2026-05-26 12:04:24 +0200

    Initialisation de la classe Batiment

---
+++ src/main/java/universite_paris8/iut/mcheema/codesource/modele/Batiment.java
@@ -0,0 +1,64 @@
+package universite_paris8.iut.mcheema.codesource.modele;
+
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+
+/**
+ * La classe Batiment s'occupe de la gestion de tours et de leur logique.
+ * Elle s'occupe de leurs comportements, de leur déplacements, améliorations...
+ */
+
+public abstract class Batiment {
+    private IntegerProperty xProperty;
+    private IntegerProperty yProperty;
+    private int prix;
+    private int niveau;
+    private int nbreAmeliorations;
+    private int degat;
+    private int vitesseAttaque;
+
+    public Batiment(int x, int y, int prix, int ameliorations, int degat, int vAttaque) {
+        this.xProperty = new SimpleIntegerProperty(x);
+        this.yProperty = new SimpleIntegerProperty(y);
+        this.prix = prix;
+        this.niveau = 1;
+        this.nbreAmeliorations = ameliorations;
+        this.degat = degat;
+        this.vitesseAttaque = vAttaque;
+    }
+
+    public final IntegerProperty xProperty() {
+        return this.xProperty;
+    }
+
+    public final IntegerProperty yProperty() {
+        return this.yProperty;
+    }
+
+    public final int getX() {
+        return this.xProperty.getValue();
+    }
+
+    public final int getY() {
+        return this.yProperty.getValue();
+    }
+
+    public final void setX(int x) {
+        this.xProperty.set(x);
+    }
+
+    public final void setY(int y) {
+        this.yProperty.set(y);
+    }
+
+    public int prixAmelioration() {
+        return (int)(this.prix * 0.3 * this.niveau);
+    }
+
+    public int prixDeplace() {
+        return (int)(this.prix * 0.4 * this.niveau);
+    }
+
+    public abstract void effectueAction();
+
+}
---
+++ src/main/java/universite_paris8/iut/mcheema/codesource/vue/BatimentVue.java
@@ -0,0 +1,21 @@
+package universite_paris8.iut.mcheema.codesource.vue;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Circle;
+import universite_paris8.iut.mcheema.codesource.modele.Batiment;
+
+/**
+ * La classe BatimentVue s'occupe de l'affichage des bâtiments, en affichant leurs sprites, leurs projectiles, etc.
+ */
+public class BatimentVue {
+    private Batiment batiment;
+    private Pane paneJeu;
+
+    public BatimentVue(Batiment bat, Pane paneJ) {
+        this.batiment = bat;
+        this.paneJeu = paneJ;
+    }
+
+    public void creerSpriteBatiment() {
+        Circle spriteB = new Circle(4);
+    }
+}
