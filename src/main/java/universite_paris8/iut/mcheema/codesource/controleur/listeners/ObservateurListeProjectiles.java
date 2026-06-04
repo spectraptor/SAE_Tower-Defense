d19ed6dc541af6e4b21fb630b51f6ac6a0db9cd1
Valentin Huang <vhuang@iut.univ-paris8.fr>
2026-06-04 16:05:31 +0200

    ajouts des images de projectiles

--- src/main/java/universite_paris8/iut/mcheema/codesource/controleur/listeners/ObservateurListeProjectiles.java
+++ src/main/java/universite_paris8/iut/mcheema/codesource/controleur/listeners/ObservateurListeProjectiles.java
@@ -1,7 +1,9 @@
 package universite_paris8.iut.mcheema.codesource.controleur.listeners;
 
+import javafx.animation.PauseTransition;
 import javafx.collections.ListChangeListener;
 import javafx.scene.layout.Pane;
+import javafx.util.Duration;
 import universite_paris8.iut.mcheema.codesource.modele.projectile.MissileZone;
 import universite_paris8.iut.mcheema.codesource.modele.projectile.Projectile;
 import universite_paris8.iut.mcheema.codesource.vue.ProjectileVue;
@@ -25,10 +27,14 @@ public class ObservateurListeProjectiles implements ListChangeListener<Projectil
                 this.paneJeu.lookup("#"+projectile.getId()).translateXProperty().unbind();
                 this.paneJeu.lookup("#"+projectile.getId()).translateYProperty().unbind();
                 this.paneJeu.getChildren().remove(this.paneJeu.lookup("#"+projectile.getId()));
-                if(projectile instanceof MissileZone) {
+
+                if (projectile instanceof MissileZone) {
                     this.paneJeu.lookup("#" + projectile.getId() + "P").translateXProperty().unbind();
                     this.paneJeu.lookup("#" + projectile.getId() + "P").translateYProperty().unbind();
-                    this.paneJeu.getChildren().remove(this.paneJeu.lookup("#" + projectile.getId() + "P"));
+                    this.paneJeu.lookup("#" + projectile.getId() + "P").setVisible(true);
+                    PauseTransition pause = new PauseTransition(Duration.millis(500));
+                    pause.setOnFinished(e -> this.paneJeu.getChildren().remove(this.paneJeu.lookup("#" + projectile.getId() + "P")));
+                    pause.play();
                 }
             }
         }
