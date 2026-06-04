8165f3ab398306315125e34bd1670af4d8249c94
Valentin Huang <vhuang@iut.univ-paris8.fr>
2026-06-04 14:35:26 +0200

    Ajout des 6 images des batiments

--- src/main/java/universite_paris8/iut/mcheema/codesource/vue/BatimentVue.java
+++ src/main/java/universite_paris8/iut/mcheema/codesource/vue/BatimentVue.java
@@ -1,8 +1,11 @@
 package universite_paris8.iut.mcheema.codesource.vue;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
-import universite_paris8.iut.mcheema.codesource.modele.batiment.Batiment;
+import universite_paris8.iut.mcheema.codesource.modele.Terrain;
+import universite_paris8.iut.mcheema.codesource.modele.batiment.*;
 
 /**
  * La classe BatimentVue s'occupe de l'affichage des bâtiments, en affichant leurs sprites, leurs projectiles, etc.
@@ -17,12 +20,30 @@ public class BatimentVue {
     }
 
     public void creerSpriteBatiment() {
-        Circle spriteB = new Circle(4, Color.DODGERBLUE);
 
-        spriteB.translateXProperty().bind(this.batiment.xProperty());
-        spriteB.translateYProperty().bind(this.batiment.yProperty());
+        if(this.batiment instanceof Compilateur) {
+            Image imgCompilateur = new Image(getClass().getResource("/universite_paris8/iut/mcheema/codesource/texture/batiments/compilateur.png").toExternalForm());
+            lier(imgCompilateur);
+        }
+        else if(this.batiment instanceof Debugger) {
+            Image imgDebugger = new Image(getClass().getResource("/universite_paris8/iut/mcheema/codesource/texture/batiments/debugger.png").toExternalForm());
+            lier(imgDebugger);
+        }
+        else if(this.batiment instanceof BombeLogique) {
+            Image imgBombeLogique = new Image(getClass().getResource("/universite_paris8/iut/mcheema/codesource/texture/batiments/bombeLogique.png").toExternalForm());
+            lier(imgBombeLogique);
+        }
+        else if(this.batiment instanceof Surcadence) {
+            Image imgSurcadence = new Image(getClass().getResource("/universite_paris8/iut/mcheema/codesource/texture/batiments/surcadence.png").toExternalForm());
+            lier(imgSurcadence);
+        }
 
-        Circle rayonB = new Circle(this.batiment.getPortee()); // permet de visualiser le rayon de la tour
+
+        // Circle spriteB = new Circle(4, Color.DODGERBLUE);
+
+        // spriteB.translateXProperty().bind(this.batiment.xProperty());
+        // spriteB.translateYProperty().bind(this.batiment.yProperty());
+        // Circle rayonB = new Circle(this.batiment.getPortee()); // permet de visualiser le rayon de la tour
 
         /* Style visuel */
         /*
@@ -33,10 +54,18 @@ public class BatimentVue {
         */
         /* Au moment ou le sprite est crée, translateX et translateY sont nuls.
         On est donc obligé de faire un bind, autrement ça ne marche pas. */
-        rayonB.centerXProperty().bind(spriteB.translateXProperty());
-        rayonB.centerYProperty().bind(spriteB.translateYProperty());
+        // rayonB.centerXProperty().bind(spriteB.translateXProperty());
+        // rayonB.centerYProperty().bind(spriteB.translateYProperty());
 
-        this.paneJeu.getChildren().add(1, spriteB);
+        // this.paneJeu.getChildren().add(1, spriteB);
         // this.paneJeu.getChildren().add(rayonB);
+    }
+
+
+    private void lier(Image img) {
+        ImageView vue = new ImageView(img);
+        vue.translateXProperty().bind(this.batiment.xProperty().subtract(Terrain.TAILLE_TUILLE/2));
+        vue.translateYProperty().bind(this.batiment.yProperty().subtract(Terrain.TAILLE_TUILLE/2));
+        this.paneJeu.getChildren().add(1, vue );
     }
 }
