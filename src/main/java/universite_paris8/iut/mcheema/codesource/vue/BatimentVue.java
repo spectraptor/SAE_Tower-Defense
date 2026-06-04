ac922602ccbbf00a74cd66e33a93c79edb456557
CHEEMA Mohammad <mcheema>
2026-06-04 16:23:25 +0200

    Ajout d'un pane lorsqu'on clique sur un batiment

--- src/main/java/universite_paris8/iut/mcheema/codesource/vue/BatimentVue.java
+++ src/main/java/universite_paris8/iut/mcheema/codesource/vue/BatimentVue.java
@@ -1,4 +1,9 @@
 package universite_paris8.iut.mcheema.codesource.vue;
+import javafx.geometry.Insets;
+import javafx.scene.control.*;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
@@ -19,6 +24,7 @@ public class BatimentVue {
     public void creerSpriteBatiment() {
         Circle spriteB = new Circle(4, Color.DODGERBLUE);
 
+        spriteB.setId(this.batiment.getId());
         spriteB.translateXProperty().bind(this.batiment.xProperty());
         spriteB.translateYProperty().bind(this.batiment.yProperty());
 
@@ -38,5 +44,49 @@ public class BatimentVue {
 
         this.paneJeu.getChildren().add(1, spriteB);
         // this.paneJeu.getChildren().add(rayonB);
+
+        //INFO SUR LA TOUR
+
+        spriteB.setOnMouseClicked(e -> {
+            Pane paneInfo = new Pane();
+            paneInfo.setId(this.batiment.getId() + "I");
+            paneInfo.setPrefWidth(200);
+            paneInfo.setPrefHeight(150);
+
+            Label textInfo = new Label();
+            textInfo.setWrapText(true);
+            textInfo.setPrefWidth(200);
+            textInfo.setPrefHeight(150);
+            textInfo.setId("textInfoBatiment");
+            textInfo.setText(this.batiment.toString());
+            textInfo.setPadding(new Insets(5,0,5,15));
+            paneInfo.getChildren().add(textInfo);
+
+            Button boutonQuitter = new Button("X");
+            boutonQuitter.setTranslateX(163);
+            boutonQuitter.setTranslateY(2);
+            boutonQuitter.setPrefWidth(30);
+            boutonQuitter.setPrefHeight(5);
+            paneInfo.getChildren().add(boutonQuitter);
+            boutonQuitter.setId("boutonQuitterInfoBat");
+
+            this.paneJeu.getChildren().add(4,paneInfo);
+
+            if(this.batiment.getX() <= this.paneJeu.getPrefWidth()/2) {
+                paneInfo.setTranslateX(batiment.getX());
+            }
+            else {
+                paneInfo.setTranslateX(batiment.getX()-paneInfo.getPrefWidth());
+            }
+            if(this.batiment.getY() <= this.paneJeu.getPrefHeight()/2) {
+                paneInfo.setTranslateY(batiment.getY());
+            }
+            else {
+                paneInfo.setTranslateY(batiment.getY()-paneInfo.getPrefHeight());
+            }
+            boutonQuitter.setOnMouseClicked(event-> {
+                this.paneJeu.getChildren().remove(paneInfo);
+            });
+        });
     }
 }
