047c4d3cbd352520f427783bd2051ef21d0fb6c0
CHEEMA Mohammad <mcheema>
2026-06-09 23:15:48 +0200

    Globalisation de la méthode effectueAction pour toutes les tours et création d'une méthode abstraite peutAttaquer pour chaque ennemi

--- src/main/java/universite_paris8/iut/mcheema/codesource/modele/batiment/Cloud.java
+++ src/main/java/universite_paris8/iut/mcheema/codesource/modele/batiment/Cloud.java
@@ -2,6 +2,11 @@ package universite_paris8.iut.mcheema.codesource.modele.batiment;
 
 import universite_paris8.iut.mcheema.codesource.modele.Environnement;
 import universite_paris8.iut.mcheema.codesource.modele.ennemi.Ennemi;
+import universite_paris8.iut.mcheema.codesource.modele.ennemi.EnnemiCammoufle;
+import universite_paris8.iut.mcheema.codesource.modele.ennemi.EnnemiVolant;
+import universite_paris8.iut.mcheema.codesource.modele.projectile.MissileFragmentation;
+import universite_paris8.iut.mcheema.codesource.modele.projectile.MissileTete;
+import universite_paris8.iut.mcheema.codesource.modele.projectile.MissileZone;
 import universite_paris8.iut.mcheema.codesource.modele.projectile.Projectile;
 
 public class Cloud extends BatimentTir {
@@ -9,14 +14,25 @@ public class Cloud extends BatimentTir {
         super("Cloud", x, y, 50, 1, 50, 100, env);
     }
 
-    @Override
-    public void effectueAction() {
 
+    @Override
+    public boolean peutAttaquer(Ennemi cible) {
+        return (!(cible instanceof EnnemiCammoufle));
     }
 
     @Override
     public Projectile choisirProjectile(Ennemi cible) {
-        return null;
+        Projectile p;
+        switch(this.getNiveau()) {
+            case 1,2,3:
+                p =  new MissileTete(this.getX(),this.getY(),this.getDegat(),cible,this.getEnvironnement());
+                break;
+            default :
+                p = new MissileFragmentation(this.getX(),this.getY(),this.getDegat(),cible,this.getEnvironnement());
+                p.setVitesse(p.getVitesse()*2);
+                break;
+        }
+        return p;
     }
 
 }
