a5a2fc1656894d7ef5566cffb0fefaecd9f813a5
CHEEMA Mohammad <mcheema>
2026-06-10 13:32:05 +0200

    Amelioration

--- src/main/java/universite_paris8/iut/mcheema/codesource/modele/GestionVague.java
+++ src/main/java/universite_paris8/iut/mcheema/codesource/modele/GestionVague.java
@@ -1,4 +1,5 @@
 package universite_paris8.iut.mcheema.codesource.modele;
+/*
 
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleIntegerProperty;
@@ -8,18 +9,20 @@ import universite_paris8.iut.mcheema.codesource.modele.ennemi.Ennemi;
 import java.util.ArrayList;
 
 public class GestionVague {
+    private final static int TEMPS_ATTENTE = 200;
     private IntegerProperty numeroVagueProperty;
     private Environnement environnement;
     private int nbreEnnemiApparu;
+    private ArrayList<Ennemi> ennemisASpawn;
     private int niveau;
-    private ArrayList<Ennemi> listeEnnemisActuel;
+    private boolean vagueCouranteFini;
 
     public GestionVague(Environnement environnement,int niveau) {
         this.numeroVagueProperty = new SimpleIntegerProperty(1);
         this.environnement = environnement;
         this.nbreEnnemiApparu = 0;
+        this.ennemisASpawn = new ArrayList<>();
         this.niveau = niveau;
-        this.listeEnnemisActuel = new ArrayList<>();
     }
 
     public final int getNumeroVague() {
@@ -34,24 +37,43 @@ public class GestionVague {
         return this.numeroVagueProperty;
     }
 
-    public void ajouterEnnemi() {
-        if(this.nbreEnnemiApparu <= this.listeEnnemisActuel.size() -1) {
-            Niveau niveauChoisi = new Niveau(this.niveau);
-
-            for (int i = 0; i < niveauChoisi.getBases().size(); i++) {
-                Point base = niveauChoisi.getBases().get(i);
-                Point entree = niveauChoisi.getEntrees().get(i);
-                BFS bfs = new BFS(this.environnement.getTerrainDeJeu(), base);
-                ArrayList<Point> chemin = bfs.cheminDepuisSource(entree);
-
-                for (int j = 0; j < 10; j++) {
-                    this.environnement.ajouterEnnemi(new Bogue(this.environnement, chemin));
-                    this.nbreEnnemiApparu++;
-                }
+    public void t() {
+        switch (this.getNumeroVague()) {
+            case 1:
+                this.vague1();
+                break;
+        }
+        if(this.environnement.getEnnemis().isEmpty() && this.ennemisASpawn.isEmpty()) {
+            this.setNumeroVague(this.getNumeroVague()+1);
+        }
+        if(this.environnement.getNbTours() % TEMPS_ATTENTE == 0) {
+            System.out.println("teee");
+            for(int i = 0;i<this.ennemisASpawn.size();i++) {
+                this.environnement.getEnnemis().add(this.ennemisASpawn.remove(i));
             }
         }
 
     }
 
+    public void vague1() {
+        if(this.nbreEnnemiApparu < 3) {
+            Niveau terrain = new Niveau(this.niveau);
+            for (int i = 0; i < terrain.getBases().size(); i++) {
+                System.out.println("te");
+                Point base = terrain.getBases().get(i);
+                Point entree = terrain.getEntrees().get(i);
+                BFS bfs = new BFS(this.environnement.getTerrainDeJeu(), base);
+                ArrayList<Point> chemin = bfs.cheminDepuisSource(entree);
+
+                for (int j = 0; j < 3; j++) {
+                    this.ennemisASpawn.add(new Bogue(this.environnement, chemin));
+                    this.nbreEnnemiApparu++;
+                }
+            }
+        }
+    }
+    
 
 }
+
+ */
