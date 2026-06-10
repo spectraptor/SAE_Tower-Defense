6714e5aa36c2dc76132ee15afaa5e0c4571d7a80
triopowa <valentinhuang27@gmail.com>
2026-06-10 18:14:21 +0200

    retirer une partie inutile

--- src/main/java/universite_paris8/iut/mcheema/codesource/controleur/listeners/ObservateurListeProjectiles.java
+++ src/main/java/universite_paris8/iut/mcheema/codesource/controleur/listeners/ObservateurListeProjectiles.java
@@ -39,13 +39,12 @@ public class ObservateurListeProjectiles implements ListChangeListener<Projectil
                     this.paneJeu.lookup("#" + projectile.getId() + "P").translateXProperty().unbind();
                     this.paneJeu.lookup("#" + projectile.getId() + "P").translateYProperty().unbind();
                     this.paneJeu.lookup("#" + projectile.getId() + "P").setVisible(true);
-                    PauseTransition pause = new PauseTransition(Duration.millis(500));
+
                     FadeTransition disparaitre = new FadeTransition(Duration.millis(500), this.paneJeu.lookup("#" + projectile.getId() + "P"));
                     RotateTransition tourner = new RotateTransition(Duration.millis(500), this.paneJeu.lookup("#" + projectile.getId() + "P"));
                     disparaitre.setToValue(0);
                     tourner.setByAngle(360);
-                    pause.setOnFinished(e -> this.paneJeu.getChildren().remove(this.paneJeu.lookup("#" + projectile.getId() + "P")));
-                    pause.play();
+                    tourner.setOnFinished(e -> this.paneJeu.getChildren().remove(this.paneJeu.lookup("#" + projectile.getId() + "P")));
                     disparaitre.play();
                     tourner.play();
                 }
