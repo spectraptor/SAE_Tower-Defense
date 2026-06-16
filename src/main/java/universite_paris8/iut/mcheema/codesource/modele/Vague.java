923c8545c6252b2a4b0b98cf731abfbd77c3587c
CHEEMA Mohammad <mcheema>
2026-06-16 09:26:14 +0200

    Choisi un chemin aléatoirement pour chaque ennemi

--- src/main/java/universite_paris8/iut/mcheema/codesource/modele/Vague.java
+++ src/main/java/universite_paris8/iut/mcheema/codesource/modele/Vague.java
@@ -2,14 +2,9 @@ package universite_paris8.iut.mcheema.codesource.modele;
 
 import universite_paris8.iut.mcheema.codesource.modele.ennemi.*;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
-/**
- * La classe Vague s'occupe de la gestion individuelle des vagues.
- * Elle a pour but de de créer des vagues, composées d'ennemis précisément choisis
- * qui vont être capable de se déplacer vers la base et posséder un chemin.
- * Le système de vague doit être automatisée et auto-fonctionnel.
- */
 public class Vague {
     private int numeroVague;
     private ArrayList<Ennemi> listeEnnemisVague;
@@ -22,65 +17,70 @@ public class Vague {
         this.initialiseVague(numNiv);
     }
 
+
     public ArrayList<Ennemi> getlisteEnnemisVague() {
         return this.listeEnnemisVague;
     }
 
-    public void ajouterEnnemiDansVague(Ennemi e) {
-        this.listeEnnemisVague.add(e);
-    }
-
     public void initialiseVague(int niveau) {
         Niveau niveauChoisi = new Niveau(niveau);
-        ArrayList<Point> chemin = null;
+        ArrayList<ArrayList<Point>> chemins = new ArrayList<>();
 
         for (int i = 0; i < niveauChoisi.getBases().size(); i++) {
             Point base = niveauChoisi.getBases().get(i);
             Point entree = niveauChoisi.getEntrees().get(i);
             BFS bfs = new BFS(this.environnement.getTerrainDeJeu(), base);
-            chemin = bfs.cheminDepuisSource(entree);
+            ArrayList<Point> chemin = bfs.cheminDepuisSource(entree);
+            chemins.add(chemin);
         }
+
 
         switch (this.numeroVague) {
             case 1:
                 // Créer 3 bogues
                 for (int i = 0; i < 3; i++) {
-                    this.ajouterEnnemiDansVague(new Bogue(this.environnement, chemin));
+                    this.listeEnnemisVague.add(new Bogue(this.environnement, this.choisiAleatoirementChemin(chemins)));
                 }
                 break;
             case 2:
                 // Créer 3 bogues, 2 ralentisseur, 1 ping
                 for (int i = 0; i < 3; i++) {
-                    this.ajouterEnnemiDansVague(new Bogue(this.environnement, chemin));
+                    this.listeEnnemisVague.add(new Bogue(this.environnement, this.choisiAleatoirementChemin(chemins)));
                 }
 
-                this.ajouterEnnemiDansVague(new Ralentisseur(this.environnement, chemin));
-                this.ajouterEnnemiDansVague(new Ralentisseur(this.environnement, chemin));
+                this.listeEnnemisVague.add(new Ralentisseur(this.environnement, this.choisiAleatoirementChemin(chemins)));
+                this.listeEnnemisVague.add(new Ralentisseur(this.environnement, this.choisiAleatoirementChemin(chemins)));
 
-                this.ajouterEnnemiDansVague(new Ping(this.environnement, chemin));
+                this.listeEnnemisVague.add(new Ping(this.environnement, this.choisiAleatoirementChemin(chemins)));
 
                 break;
 
             case 3:
                 // Créer 5 bogues
                 for (int i = 0; i < 5; i++) {
-                    this.ajouterEnnemiDansVague(new Bogue(this.environnement, chemin));
+                    this.listeEnnemisVague.add(new Bogue(this.environnement, this.choisiAleatoirementChemin(chemins)));
                 }
                 break;
             case 4:
                 // Créer 3 bogues, 2 ping, 1 ralentisseur, 2 bosses.
                 for (int i = 0; i < 3; i++) {
-                    this.ajouterEnnemiDansVague(new Bogue(this.environnement, chemin));
+                    this.listeEnnemisVague.add(new Bogue(this.environnement, this.choisiAleatoirementChemin(chemins)));
                 }
 
-                this.ajouterEnnemiDansVague(new Ping(this.environnement, chemin));
-                this.ajouterEnnemiDansVague(new Ping(this.environnement, chemin));
+                this.listeEnnemisVague.add(new Ping(this.environnement, this.choisiAleatoirementChemin(chemins)));
+                this.listeEnnemisVague.add(new Ping(this.environnement, this.choisiAleatoirementChemin(chemins)));
 
-                this.ajouterEnnemiDansVague(new Ralentisseur(this.environnement, chemin));
+                this.listeEnnemisVague.add(new Ralentisseur(this.environnement, this.choisiAleatoirementChemin(chemins)));
 
-                this.ajouterEnnemiDansVague(new ErreurDeLogique(this.environnement, chemin));
-                this.ajouterEnnemiDansVague(new ErreurDeSyntaxe(this.environnement, chemin));
+                this.listeEnnemisVague.add(new ErreurDeLogique(this.environnement, this.choisiAleatoirementChemin(chemins)));
+                this.listeEnnemisVague.add(new ErreurDeSyntaxe(this.environnement, this.choisiAleatoirementChemin(chemins)));
+
         }
+    }
+
+    public ArrayList<Point> choisiAleatoirementChemin(ArrayList<ArrayList<Point>> chemins) {
+        int numeroChemin = (int) (Math.random() * chemins.size());
+        return chemins.get(numeroChemin);
     }
 
 }
